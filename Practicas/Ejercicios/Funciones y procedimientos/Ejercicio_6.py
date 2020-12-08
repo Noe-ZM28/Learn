19ca3ec3f6ecda2d0a78e2d6214b8848569af17e
Noe Zuñiga Morales <noe.zm2000@gmail.com>
2020-12-08 15:10:45 -0600

    Empezando el ejercicios de la base de datos

--- Practicas/Ejercicios/Funciones y procedimientos/Ejercicio_6.py
+++ Practicas/Ejercicios/Funciones y procedimientos/Ejercicio_6.py
@@ -1,10 +1,8 @@
 from tkinter import *
 from tkinter import ttk, messagebox
-
 ventana = Tk()
 ventana.title("Lista")
 ventana.geometry("300x200")
-
 a1 = 10
 a2= 30
 a3 = 40
@@ -12,11 +10,9 @@ a4 = 80
 a5 = 98
 cantidad = IntVar()
 def comando():
-    
     if lista.get() == "Articulo 1":
 
         messagebox.showinfo("Resultado", f"Articulo: {lista.current()}, cantidad {2}, total 20")
-
 opciones = [
             "Articulo 1",
             "Articulo 2",
@@ -29,6 +25,4 @@ lista["values"] = opciones
 cantidad = Label(ventana, text= "Catidad").place(x = 10, y = 35)
 Button(ventana, text="Seleccionar", command= comando).place(x = 165, y = 7)
 caja = Entry(ventana, textvariable = cantidad).place(x = 100, y = 38)
-
-
 ventana.mainloop()
